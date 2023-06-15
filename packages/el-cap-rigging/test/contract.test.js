ba28c73777f7caca000567a13b5d21a5286cc5c2
Luke Annison <lukeannison@gmail.com>
2023-06-15 18:06:58 +0100

    feat: stubs with before and after tests back in

--- packages/el-cap-rigging/test/contract.test.js
+++ packages/el-cap-rigging/test/contract.test.js
@@ -1,10 +1,59 @@
 import { suite } from "uvu";
 import * as assert from "uvu/assert";
-// ... Other imports
+import { WarpFactory, LoggerFactory } from "warp-contracts/mjs";
+import { DeployPlugin } from "warp-contracts-plugin-deploy";
+import ArLocal from "arlocal";
+import * as fs from "fs";
+
+// Things that need to be used in multiple blocks
+// for example, we start arlocal in the .before() and stop it in the .after()
+let warp;
+let wallet1;
+let connectedWallet1;
+let arlocal;
 
 const test = suite("contract");
 
-// ... Setup code
+test.before(async () => {
+  arlocal = new ArLocal.default();
+  await arlocal.start();
+  LoggerFactory.INST.logLevel("error");
+  warp = WarpFactory.forLocal().use(new DeployPlugin());
+
+  // Generate a wallet
+  wallet1 = await warp.generateWallet();
+
+  // Grab the contract and initial state
+  const prefix = `./dist/`;
+  const contractSrc = fs.readFileSync(`${prefix}contract.js`, "utf8");
+  const state = JSON.parse(
+    fs.readFileSync(`${prefix}initial-state.json`, "utf8")
+  );
+
+  // Update initial state giving the wallet 100
+  const initialState = {
+    ...state,
+    ...{
+      owner: wallet1.address,
+      balances: {
+        [wallet1.address]: 100,
+      },
+    },
+  };
+
+  // Deploy contract
+  const { contractTxId } = await warp.deploy({
+    wallet: wallet1.jwk,
+    initState: JSON.stringify(initialState),
+    src: contractSrc,
+  });
+
+  // Connect wallet to contract
+  connectedWallet1 = warp
+    .contract(contractTxId)
+    .setEvaluationOptions({ internalWrites: true, mineArLocalBlocks: true })
+    .connect(wallet1.jwk);
+});
 
 test("should add a coin", async () => {
   // TODO: Implement test
@@ -38,6 +87,8 @@ test("should delete a coin", async () => {
   // TODO: Implement test
 });
 
-// ... Teardown code
+test.after(async () => {
+  await arlocal.stop();
+});
 
 test.run();
