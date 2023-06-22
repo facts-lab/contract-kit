a58e8da9dd45683375c79dbeac1f876281d8c891
Justin Shaw <mogulx_operates@yahoo.com>
2023-06-22 16:53:08 -0500

    refactor: setup env and funcs

--- packages/fact-market/src/contract.js
+++ packages/fact-market/src/contract.js
@@ -1,26 +1,28 @@
 import { balance } from './read/balance.js';
-import { transfer } from './write/transfer.js';
-import { claim } from './write/claim.js';
-import { allow } from './write/allow.js';
-import { rejectClaimable } from './write/reject.js';
+import { buy } from './write/buy.js';
+import { sell } from './write/sell.js';
 
 export async function handle(state, action) {
   // Only allow L2 transactions
-  if (SmartWeave.transaction.origin !== 'L2') {
-    return { state };
-  }
+  // if (SmartWeave.transaction.origin !== 'L2') {
+  //   return { state };
+  // }
+
+  const env = {
+    contract: SmartWeave.contract,
+    contracts: SmartWeave.contracts,
+    transaction: SmartWeave.transaction,
+  };
 
   switch (action?.input?.function) {
     case 'balance':
       return balance(state, action);
-    case 'reject':
-      return rejectClaimable(state, action);
-    case 'transfer':
-      return transfer(state, action);
-    case 'allow':
-      return allow(state, action);
-    case 'claim':
-      return claim(state, action);
+    case 'buy':
+      return buy(env)(state, action);
+    case 'sell':
+      return sell(env)(state, action);
+    case 'get-price':
+      throw new ContractError('Implement me please!');
     default:
       throw new ContractError(
         `No function supplied or function not recognized`
