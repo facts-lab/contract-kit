2cf4c41a1e14b5e0b59c54b0cbd737c6bfb74e74
Justin Shaw <mogulx_operates@yahoo.com>
2023-06-22 16:54:20 -0500

    refactor: make synchronous to be called by FACTS

--- packages/fact-market/src/write/buy.js
+++ packages/fact-market/src/write/buy.js
@@ -1,15 +1,10 @@
-import Async from 'hyper-async';
-const { fromPromise } = Async;
 import { of as syncOf, fromNullable } from '../hyper-either.js';
 import {
   POSITION_TYPES,
-  calculatePrice,
   ce,
-  getBalances,
-  getCurrentSupply,
-  roundUp,
   roundDown,
   isInteger,
+  getPriceAndFee,
 } from '../util.js';
 
 /**
@@ -20,47 +15,18 @@ import {
  * @param {*} { write, transaction }
  * @return {*}
  */
-export function buy({ contracts, transaction }) {
-  return async (state, action) =>
-    Async.of({ state, action })
+export function buy({ contract }) {
+  /**
+   * This function should only be called by the FACTS contract as action.caller
+   */
+  return (state, action) =>
+    syncOf({ state, action, contract })
       .map(validate)
-      .map(getPriceAndFee)
-      .chain(({ price, fee }) =>
-        fromPromise(claimPair)({
-          price,
-          fee,
-          pair: state.pair,
-          tx: action.input.tx,
-          contracts,
-        })
-      )
-      // Next 4 steps Only rejects if input is "error" (result from write / claimPair)
-      .chain(({ type, price, fee }) =>
-        fromPromise(rejectClaim)({
-          type,
-          price,
-          fee,
-          pair: state?.pair,
-          tx: transaction.id,
-          contracts,
-        })
-      )
-      .map(({ type, price, fee }) =>
-        creatorCut({ type, price, fee, state, action })
-      )
+      .map(validatePriceAndFee)
       .map(({ type, price, fee }) =>
         addBalance({ type, price, fee, state, action })
       )
-      .chain(({ type, price, fee }) =>
-        fromPromise(registerInteraction)({
-          type,
-          price,
-          fee,
-          contracts,
-          tx: transaction.id,
-        })
-      )
-      .fork(
+      .fold(
         (msg) => {
           throw new ContractError(msg || 'An error occurred.');
         },
@@ -74,13 +40,13 @@ export function buy({ contracts, transaction }) {
  * @author @jshaw-ar
  * @param {*} { state, action }
  */
-const validate = ({ state, action }) =>
+const validate = ({ state, action, contract }) =>
   syncOf({ state, action })
     .chain(fromNullable)
     .chain(
       ce(
-        !POSITION_TYPES.includes(action?.input?.positionType),
-        'positionType must be support or oppose.'
+        !POSITION_TYPES.includes(action?.input?.position),
+        'position must be support or oppose.'
       )
     )
     .chain(
@@ -95,15 +61,23 @@ const validate = ({ state, action }) =>
         'qty must be an integer greater than zero.'
       )
     )
-    .chain(ce(!action?.input?.tx, 'tx is required.'))
+    .chain(
+      ce(
+        state.position !== action?.input?.owner?.position,
+        `owner position must be ${state.position}.`
+      )
+    )
+    .chain(
+      ce(
+        contract.owner !== action?.input?.owner?.addr,
+        `owner addr must be ${contract.owner}.`
+      )
+    )
     .fold(
       (msg) => {
         throw new ContractError(msg || 'An error occurred.');
       },
-      () => ({
-        state,
-        action,
-      })
+      (output) => output
     );
 
 /**
@@ -113,16 +87,9 @@ const validate = ({ state, action }) =>
  * @param {*} { state, action }
  * @return {*}
  */
-const getPriceAndFee = ({ state, action }) => {
+const validatePriceAndFee = ({ state, action }) => {
   return syncOf({ state, action })
-    .map(getBalances)
-    .map(getCurrentSupply)
-    .map((supply) =>
-      roundUp(
-        calculatePrice(1, 1, supply, supply + roundDown(action.input.qty))
-      )
-    )
-    .map(getFee)
+    .map(getPriceAndFee)
     .chain(({ price, fee }) =>
       ce(price !== action.input.price, 'Incorrect price.')({ price, fee })
     )
@@ -138,59 +105,6 @@ const getPriceAndFee = ({ state, action }) => {
 };
 
 /**
- * @desription Uses Foreign Call Protocol to claim a transfer from the pair.
- *
- * @author @jshaw-ar
- * @param {*} { price, fee, state, action, write, transaction }
- * @returns {*} { price, fee, state, action, write, transaction }
- */
-const claimPair = async ({ price, fee, pair, tx, contracts }) => {
-  const result = await contracts.write(pair, {
-    function: 'claim',
-    txID: tx,
-    qty: price + fee,
-  });
-  return {
-    type: result.type,
-    price,
-    fee,
-  };
-};
-
-/**
- * @desription Uses Foreign Call Protocol to claim a transfer from the pair.
- *
- * @author @jshaw-ar
- * @param {*} { price, fee, state, action, write, type, transaction }
- * @returns {*} { price, fee, state, action, write, type, transaction }
- */
-const rejectClaim = async ({ type, price, fee, pair, tx, contracts }) => {
-  if (type === 'error') {
-    await contracts.write(pair, {
-      function: 'reject',
-      tx,
-    });
-  }
-  return { type, price, fee };
-};
-
-/**
- * @description Adds fee to the creator balance if positionType is the same as state.position
- *
- * @author @jshaw-ar
- * @param {*} { price, fee, state, action, write, type, transaction }
- * @return {*} { price, fee, state, action, write, type, transaction }
- */
-const creatorCut = ({ type, price, fee, state, action }) => {
-  if (type === 'ok') {
-    if (action.input.positionType === state.position) {
-      state.creator_cut = (state.creator_cut || 0) + fee;
-    }
-  }
-  return { type, price, fee };
-};
-
-/**
  * Adds the support or opposition balance of the caller
  *
  * @author @jshaw-ar
@@ -198,49 +112,14 @@ const creatorCut = ({ type, price, fee, state, action }) => {
  * @return {*} { price, fee, state, action, write, type, transaction }
  */
 const addBalance = ({ type, price, fee, state, action }) => {
-  if (type === 'ok') {
-    if (action.input.positionType === 'support') {
-      state.balances[action.caller] =
-        (state.balances[action.caller] || 0) + action.input.qty;
-    } else {
-      state.oppositionBalances[action.caller] =
-        (state.oppositionBalances[action.caller] || 0) + action.input.qty;
-    }
+  if (action.input.position === 'support') {
+    state.balances[action.caller] = roundDown(
+      (state.balances[action.caller] || 0) + action.input.qty
+    );
+  } else {
+    state.oppositionBalances[action.caller] = roundDown(
+      (state.oppositionBalances[action.caller] || 0) + action.input.qty
+    );
   }
   return { type, price, fee };
 };
-
-/**
- * @description Registers interaction with the registry contract
- *
- * @author @jshaw-ar
- * @param {*} {
- *   price,
- *   fee,
- *   state,
- *   write,
- *   transaction,
- *   type,
- * }
- */
-const registerInteraction = async ({ type, price, fee, contracts, tx }) => {
-  if (type === 'ok') {
-    await contracts.write('<FACTS_CONTRACT_ID>', {
-      function: 'register',
-      tx: tx,
-      qty: price + fee,
-    });
-  }
-};
-
-/**
- * Calculates fee
- *
- * @author @jshaw-ar
- * @param {*} price
- * @returns {Object} {fee, price}
- */
-const getFee = (price) => ({
-  fee: roundUp((5 / 100) * price),
-  price,
-});
