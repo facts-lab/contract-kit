5ac66f150abae01ca0572b65bfb7b982de7b80dd
Justin Shaw <mogulx_operates@yahoo.com>
2023-06-27 13:02:12 -0500

    test: live integration unit testing (lol)

--- packages/facts-token/test/full-integration.test.js
+++ packages/facts-token/test/full-integration.test.js
@@ -1,242 +1,246 @@
-import { suite } from 'uvu';
-import * as assert from 'uvu/assert';
-import { WarpFactory, LoggerFactory, SourceType } from 'warp-contracts/mjs';
-import { DeployPlugin } from 'warp-contracts-plugin-deploy';
-import ArLocal from 'arlocal';
-import * as fs from 'fs';
-import { toPairs } from 'ramda';
+// import { suite } from 'uvu';
+// import * as assert from 'uvu/assert';
+// import { WarpFactory, LoggerFactory, SourceType } from 'warp-contracts/mjs';
+// import { DeployPlugin } from 'warp-contracts-plugin-deploy';
+// import ArLocal from 'arlocal';
+// import * as fs from 'fs';
+// import { toPairs } from 'ramda';
 
-// Things that need to be used in multiple blocks
-// for example, we start arlocal in the .before() and stop it in the .after()
-let warp;
-let wallet1;
-let connectedWallet1L1;
-let connectedWallet1SEQ;
-let wallet2;
-let connectedWallet2L1;
-let connectedWallet2SEQ;
-let arlocal;
-// This will be set after allow, and be used for claiming
-let allowTxForClaim1;
-let allowTxForClaim2;
+// // What am I testing?
+// /**
+//  * 1. Deploy U token
+//  * 2. Deploy Facts Token with U in state
+//  * 3. Deploy Fact Market with U pair and Facts Token in register function
+//  * 4. Run buy on facts token and log state of U in the Facts Token
+//  */
 
-const test = suite('full-integration');
+// // Things that need to be used in multiple blocks
+// // for example, we start arlocal in the .before() and stop it in the .after()
+// let warp;
+// let wallet1;
+// let connectedWallet1U;
+// let connectedWallet1FactsToken;
+// let connectedWallet1FactMarket;
+// let wallet2;
+// let connectedWallet2U;
+// let connectedWallet2FactsToken;
+// let connectedWallet2FactMarket;
+// let arlocal;
+// // This will be set after allow, and be used for claiming
+// let allowTxForClaim1;
+// let allowTxForClaim2;
 
-test.before(async () => {
-  arlocal = new ArLocal.default();
-  await arlocal.start();
-  LoggerFactory.INST.logLevel('error');
-  warp = WarpFactory.forLocal().use(new DeployPlugin());
+// //
+// let contractU;
+// let contractFactsToken;
+// let contractFactMarket;
+// const test = suite('full-integration');
 
-  // Generate a wallet
-  wallet1 = await warp.generateWallet();
-  wallet2 = await warp.generateWallet();
+// test.before(async () => {
+//   arlocal = new ArLocal.default();
+//   await arlocal.start();
+//   LoggerFactory.INST.logLevel('error');
+//   warp = WarpFactory.forLocal().use(new DeployPlugin());
 
-  // Grab the contract and initial state
-  const prefix = `./dist/`;
-  const integrationsPrefux = `./contract-integrations/`;
-  const contractSrcFactsToken = fs.readFileSync(`${prefix}contract.js`, 'utf8');
-  const contractSrcU = fs.readFileSync(
-    `${integrationsPrefux}u/contract.js`,
-    'utf8'
-  );
-  const contractSrcFactMarket = fs.readFileSync(
-    `${integrationsPrefux}fact-market/contract.js`,
-    'utf8'
-  );
+//   // Generate a wallet
+//   wallet1 = await warp.generateWallet();
+//   wallet2 = await warp.generateWallet();
 
-  const initialStateFactsToken = JSON.parse(
-    fs.readFileSync(`${prefix}initial-state.json`, 'utf8')
-  );
-  const initialStateU = JSON.parse(
-    fs.readFileSync(`${integrationsPrefux}u/initial-state.json`, 'utf8')
-  );
-  const initialStateFactMarket = JSON.parse(
-    fs.readFileSync(
-      `${integrationsPrefux}fact-market/initial-state.json`,
-      'utf8'
-    )
-  );
-
-  console.log('TEST');
-
-  // // Update initial state giving the wallet 100
-  // const stateL1 = {
-  //   ...initialStateL1,
-  //   ...{
-  //     owner: wallet1.address,
-  //     requests: [
-  //       {
-  //         tx: '<expired>',
-  //         target: '<jshaw>',
-  //         qty: 100,
-  //         expires: 0,
-  //       },
-  //     ],
-  //   },
-  // };
-
-  // const stateSEQ = {
-  //   ...initialStateSEQ,
-  //   ...{
-  //     owner: wallet1.address,
-  //     balances: {},
-  //     pile: { processed: 10, processed_expired: -1 },
-  //   },
-  // };
-
-  // // Deploy contract
-  // const contractL1 = await warp.deploy({
-  //   wallet: wallet1.jwk,
-  //   initState: JSON.stringify(stateL1),
-  //   src: contractSrcL1,
-  // });
-
-  // // Deploy contract
-  // const contractSEQ = await warp.deploy({
-  //   wallet: wallet1.jwk,
-  //   initState: JSON.stringify({
-  //     ...stateSEQ,
-  //     // Set the mint contract to the L1 contract tx.
-  //     mint_contract: contractL1.contractTxId,
-  //     pile: [{ processed: -1 }],
-  //   }),
-  //   src: contractSrcSEQ,
-  //   evaluationManifest: {
-  //     evaluationOptions: {
-  //       sourceType: SourceType.WARP_SEQUENCER,
-  //       internalWrites: true,
-  //       unsafeClient: 'skip',
-  //     },
-  //   },
-  // });
-
-  // // Connect wallet to contract
-  // connectedWallet1L1 = warp
-  //   .contract(contractL1.contractTxId)
-  //   .setEvaluationOptions({ internalWrites: true, mineArLocalBlocks: true })
-  //   .connect(wallet1.jwk);
-  // connectedWallet1SEQ = warp
-  //   .contract(contractSEQ.contractTxId)
-  //   .setEvaluationOptions({
-  //     internalWrites: true,
-  //     unsafeClient: 'skip',
-  //   })
-  //   .connect(wallet1.jwk);
-
-  // connectedWallet2L1 = warp
-  //   .contract(contractL1.contractTxId)
-  //   .setEvaluationOptions({ internalWrites: true, mineArLocalBlocks: true })
-  //   .connect(wallet2.jwk);
-
-  // connectedWallet2SEQ = warp
-  //   .contract(contractSEQ.contractTxId)
-  //   .setEvaluationOptions({
-  //     internalWrites: true,
-  //     unsafeClient: 'skip',
-  //   })
-  //   .connect(wallet2.jwk);
-
-  // // Mint AR to wallet1
-  // await fetch(
-  //   // Mints 300 AR.
-  //   `http://localhost:1984/mint/${wallet1.address}/100000000000000`
-  // );
-});
-
-// test('should create mint request for 10 RebAR with wallet1', async () => {
-//   const interaction = await connectedWallet1L1.writeInteraction(
-//     { function: 'create-mint' },
-//     { reward: '10000000000000' }
+//   // Grab the contract and initial state
+//   const prefix = `./dist/`;
+//   const integrationsPrefux = `./contract-integrations/`;
+//   const contractSrcFactsToken = fs.readFileSync(`${prefix}contract.js`, 'utf8');
+//   const contractSrcU = fs.readFileSync(
+//     `${integrationsPrefux}u/contract.js`,
+//     'utf8'
 //   );
-//   const state = (await connectedWallet1L1.readState()).cachedValue.state;
-//   const request = state.requests.filter(
-//     (r) => r.tx === interaction.originalTxId
-//   )[0];
-//   assert.is(request?.qty, 10000000);
-// });
+//   const contractSrcFactMarket = fs.readFileSync(
+//     `${integrationsPrefux}fact-market/contract.js`,
+//     'utf8'
+//   );
 
-// test('should mint 10 RebAR', async () => {
-//   await connectedWallet1SEQ.writeInteraction({
-//     function: 'mint',
+//   const initialStateFactsToken = JSON.parse(
+//     fs.readFileSync(`${prefix}initial-state.json`, 'utf8')
+//   );
+//   const initialStateU = JSON.parse(
+//     fs.readFileSync(`${integrationsPrefux}u/initial-state.json`, 'utf8')
+//   );
+//   const initialStateFactMarket = JSON.parse(
+//     fs.readFileSync(
+//       `${integrationsPrefux}fact-market/initial-state.json`,
+//       'utf8'
+//     )
+//   );
+
+//   // Deploy contracts
+//   contractU = await warp.deploy({
+//     wallet: wallet1.jwk,
+//     initState: JSON.stringify({
+//       ...initialStateU,
+//       balances: {
+//         [wallet1.address]: 100000000,
+//         [wallet2.address]: 100000000,
+//       },
+//     }),
+//     src: contractSrcU,
 //   });
-//   const state = (await connectedWallet1SEQ.readState()).cachedValue.state;
-//   assert.is(state.balances[wallet1.address], 10000000);
-//   const pile = toPairs(state.pile);
-//   assert.is(pile.length, 1);
-// });
 
-// test('transfer 5 to wallet 2', async () => {
-//   await connectedWallet1SEQ.writeInteraction({
-//     function: 'transfer',
-//     target: wallet2.address,
-//     qty: 5000000,
+//   contractFactsToken = await warp.deploy({
+//     wallet: wallet1.jwk,
+//     initState: JSON.stringify({
+//       ...initialStateFactsToken,
+//       u: contractU.contractTxId,
+//     }),
+//     src: contractSrcFactsToken,
 //   });
-//   const state = (await connectedWallet1SEQ.readState()).cachedValue.state;
-//   assert.is(state.balances[wallet2.address], 5000000);
-// });
 
-// test('allow 2 with wallet 2 (to wallet 1) - allow 1 with wallet 2', async () => {
-//   const interaction1 = await connectedWallet2SEQ.writeInteraction({
+//   contractFactMarket = await warp.deploy({
+//     wallet: wallet1.jwk,
+//     initState: JSON.stringify({
+//       ...initialStateFactMarket,
+//       pair: contractU.contractTxId,
+//       balances: {
+//         '<jshaw>': 99,
+//       },
+//     }),
+//     src: contractSrcFactMarket.replace(
+//       '<FACTS_CONTRACT_ID>',
+//       contractFactsToken.contractTxId
+//     ),
+//   });
+
+//   // Connect wallet to contracts
+//   connectedWallet1U = warp
+//     .contract(contractU.contractTxId)
+//     .setEvaluationOptions({
+//       internalWrites: true,
+//       mineArLocalBlocks: true,
+//       unsafeClient: 'skip',
+//     })
+//     .connect(wallet1.jwk);
+//   connectedWallet1FactsToken = warp
+//     .contract(contractFactsToken.contractTxId)
+//     .setEvaluationOptions({
+//       internalWrites: true,
+//       mineArLocalBlocks: true,
+//       unsafeClient: 'skip',
+//     })
+//     .connect(wallet1.jwk);
+//   connectedWallet1FactMarket = warp
+//     .contract(contractFactMarket.contractTxId)
+//     .setEvaluationOptions({
+//       internalWrites: true,
+//       mineArLocalBlocks: true,
+//       unsafeClient: 'skip',
+//     })
+//     .connect(wallet1.jwk);
+
+//   // Mint AR to wallets
+//   await fetch(`http://localhost:1984/mint/${wallet1.address}/100000000000000`);
+//   await fetch(`http://localhost:1984/mint/${wallet2.address}/100000000000000`);
+// });
+// /**
+//  * 1. allow to fact market
+//  * 2. buy on fact market
+//  * 3. log state of U token in Facts Token
+//  */
+// test('should allow 105 sub units to the Fact Market contract', async () => {
+//   const interaction = await connectedWallet1U.writeInteraction({
 //     function: 'allow',
-//     target: wallet1.address,
-//     qty: 2000000,
+//     target: contractFactMarket.contractTxId,
+//     qty: 2,
 //   });
 
-//   const interaction2 = await connectedWallet2SEQ.writeInteraction({
-//     function: 'allow',
-//     target: wallet1.address,
-//     qty: 1000000,
-//   });
-
-//   const state = (await connectedWallet1SEQ.readState()).cachedValue.state;
-//   assert.is(state.claimable.length, 2);
-//   // Set the allow tx for claim next
-//   allowTxForClaim1 = interaction1.originalTxId;
-//   allowTxForClaim2 = interaction2.originalTxId;
+//   const state = (await connectedWallet1U.readState()).cachedValue.state;
+//   allowTxForClaim1 = interaction.originalTxId;
+//   assert.is(state.claimable.length, 1);
 // });
 
-// test('claim 2 with wallet 1', async () => {
-//   await connectedWallet1SEQ.writeInteraction({
-//     function: 'claim',
-//     txID: allowTxForClaim1,
-//     qty: 2000000,
-//   });
-//   const state = (await connectedWallet1SEQ.readState()).cachedValue.state;
+// // test('should buy 1 position token', async () => {
+// //   await connectedWallet1FactMarket.writeInteraction({
+// //     function: 'buy',
+// //     qty: 1,
+// //     price: 100,
+// //     fee: 5,
+// //     tx: allowTxForClaim1,
+// //     position: 'support',
+// //   });
+// //   const state = (await connectedWallet1FactMarket.readState()).cachedValue
+// //     .state;
+// //   assert.is(state.balances[wallet1.address], 1);
+// // });
 
-//   assert.is(state.balances[wallet1.address], 7000000);
-//   assert.is(state.balances[wallet2.address], 2000000);
+// // test('transfer 5 to wallet 2', async () => {
+// //   await connectedWallet1SEQ.writeInteraction({
+// //     function: 'transfer',
+// //     target: wallet2.address,
+// //     qty: 5000000,
+// //   });
+// //   const state = (await connectedWallet1SEQ.readState()).cachedValue.state;
+// //   assert.is(state.balances[wallet2.address], 5000000);
+// // });
+
+// // test('allow 2 with wallet 2 (to wallet 1) - allow 1 with wallet 2', async () => {
+// //   const interaction1 = await connectedWallet2SEQ.writeInteraction({
+// //     function: 'allow',
+// //     target: wallet1.address,
+// //     qty: 2000000,
+// //   });
+
+// //   const interaction2 = await connectedWallet2SEQ.writeInteraction({
+// //     function: 'allow',
+// //     target: wallet1.address,
+// //     qty: 1000000,
+// //   });
+
+// //   const state = (await connectedWallet1SEQ.readState()).cachedValue.state;
+// //   assert.is(state.claimable.length, 2);
+// //   // Set the allow tx for claim next
+// //   allowTxForClaim1 = interaction1.originalTxId;
+// //   allowTxForClaim2 = interaction2.originalTxId;
+// // });
+
+// // test('claim 2 with wallet 1', async () => {
+// //   await connectedWallet1SEQ.writeInteraction({
+// //     function: 'claim',
+// //     txID: allowTxForClaim1,
+// //     qty: 2000000,
+// //   });
+// //   const state = (await connectedWallet1SEQ.readState()).cachedValue.state;
+
+// //   assert.is(state.balances[wallet1.address], 7000000);
+// //   assert.is(state.balances[wallet2.address], 2000000);
+// // });
+
+// // test('should claim with different txID', async () => {
+// //   await connectedWallet1SEQ.writeInteraction({
+// //     function: 'claim',
+// //     txID: allowTxForClaim2,
+// //     qty: 1000000,
+// //   });
+// //   const state = (await connectedWallet1SEQ.readState()).cachedValue.state;
+// //   assert.is(state.balances[wallet1.address], 8000000);
+// //   assert.is(state.balances[wallet2.address], 2000000);
+// // });
+
+// // test('check balance with target', async () => {
+// //   const interaction = await connectedWallet1SEQ.viewState({
+// //     function: 'balance',
+// //     target: wallet2.address,
+// //   });
+
+// //   assert.is(interaction.result.balance, 2000000);
+// // });
+
+// // test('check balance without target', async () => {
+// //   const interaction = await connectedWallet1SEQ.viewState({
+// //     function: 'balance',
+// //   });
+// //   assert.is(interaction.result.balance, 8000000);
+// // });
+
+// test.after(async () => {
+//   await arlocal.stop();
 // });
 
-// test('should claim with different txID', async () => {
-//   await connectedWallet1SEQ.writeInteraction({
-//     function: 'claim',
-//     txID: allowTxForClaim2,
-//     qty: 1000000,
-//   });
-//   const state = (await connectedWallet1SEQ.readState()).cachedValue.state;
-//   assert.is(state.balances[wallet1.address], 8000000);
-//   assert.is(state.balances[wallet2.address], 2000000);
-// });
-
-// test('check balance with target', async () => {
-//   const interaction = await connectedWallet1SEQ.viewState({
-//     function: 'balance',
-//     target: wallet2.address,
-//   });
-
-//   assert.is(interaction.result.balance, 2000000);
-// });
-
-// test('check balance without target', async () => {
-//   const interaction = await connectedWallet1SEQ.viewState({
-//     function: 'balance',
-//   });
-//   assert.is(interaction.result.balance, 8000000);
-// });
-
-test.after(async () => {
-  await arlocal.stop();
-});
-
-test.run();
+// test.run();
