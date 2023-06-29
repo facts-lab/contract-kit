636d5780fc7f1d1857eb664ac72a69576242fd85
Justin Shaw <mogulx_operates@yahoo.com>
2023-06-29 11:31:33 -0500

    refactor: delete old fns; add validateTransfer for allow / transfer

--- packages/facts-token/src/util.js
+++ packages/facts-token/src/util.js
@@ -1,29 +1,5 @@
 import BigNumber from 'bignumber.js';
 import { Left, Right } from './hyper-either.js';
-import { Rejected, Resolved } from 'hyper-async';
-
-/**
- * @description Contract Error (use with hyper-either)
- *
- * @author @jshaw-ar
- * @param {Boolean} flag What your conditional check is
- * @param {string} message Error message if conditional is true
- * @param {{state, action}} p The payload to pass through the func
- * @return {{state, action}} p
- */
-export const ce = (flag, message) => (p) => flag ? Left(message) : Right(p);
-
-/**
- * @description Contract Error Async (use with hyper-async)
- *
- * @author @jshaw-ar
- * @param {Boolean} flag What your conditional check is
- * @param {string} message Error message if conditional is true
- * @param {{state, action}} p The payload to pass through the func
- * @return {{state, action}} p
- */
-export const ceAsync = (flag, message) => (p) =>
-  flag ? Rejected(message) : Resolved(p);
 
 /**
  * @description Uses BigNumber to check if value is an integer.
@@ -62,57 +38,6 @@ export const removeExpired = (queue, height) =>
   queue.filter((request) => request?.expires > height);
 
 /**
- *
- *
- * @author @jshaw-ar
- * @param {*} target
- * @param {*} balance
- * @param {*} kv
- */
-export const getTargetBalance = async (target, balance, kv) => ({
-  balance,
-  targetBalance: (await kv.get(target)) || 0,
-});
-
-/**
- * @description Adds the qty to the 'to' balance
- *
- * @author @jshaw-ar
- * @param {*} state
- * @param {*} action
- * @param {*} kv
- * @return {*}
- */
-export const addClaimBalanceTo = ({ state, action }) => {
-  const indexToRemove = state.claimable.findIndex(
-    (claim) => claim.txID === action.input.txID
-  );
-  const claim = state.claimable[indexToRemove];
-  const balance = state.balances[claim.to] || 0;
-  state.balances[claim.to] = balance + claim.qty;
-  return indexToRemove;
-};
-
-/**
- * @description Adds the qty to the 'from' balance
- *
- * @author @jshaw-ar
- * @param {*} state
- * @param {*} action
- * @param {*} kv
- * @return {*}
- */
-export const addClaimBalanceFrom = ({ state, action }) => {
-  const indexToRemove = state.claimable.findIndex(
-    (claim) => claim.txID === action.input.tx
-  );
-  const claim = state.claimable[indexToRemove];
-  const balance = state.balances[claim.from] || 0;
-  state.balances[claim.from] = balance + claim.qty;
-  return indexToRemove;
-};
-
-/**
  * Validates a qty is an integer grater than 0
  *
  * @author @jshaw-ar
@@ -124,4 +49,22 @@ export const isValidQty = (qty) => {
   if (!isInteger(qty)) return false;
   if (qty < 1) return false;
   return true;
+};
+
+/**
+ * Validates the transfer input
+ *
+ * @author @jshaw-ar
+ * @param {*} { state, action }
+ * @return {*} { state, action }
+ */
+export const validateTransfer = ({ state, action, transaction }) => {
+  if (!action.input?.target) return Left('Please specify a target.');
+  if (action.input?.target === action.caller)
+    return Left('Target cannot be caller.');
+  if (!isValidQty(action?.input?.qty))
+    return Left('qty must be an integer greater than zero.');
+  if ((state.balances[action.caller] || 0) < roundDown(action.input?.qty))
+    return Left('Not enough tokens for transfer.');
+  return Right({ state, action, transaction });
 };
