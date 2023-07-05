2b0f2b8ecae4ce57e8d7759b0d7673b84f299581
Justin Shaw <mogulx_operates@yahoo.com>
2023-07-05 06:42:51 -0500

    test: finish sell

--- packages/fact-market/test/sell.test.js
+++ packages/fact-market/test/sell.test.js
@@ -6,298 +6,227 @@ import { setupSmartWeaveEnv } from './setup.js';
 
 const test = suite('sell');
 
-test('should throw (position must be support or oppose.) if undefined', () => {
+test('should throw (position must be support or oppose.) if undefined', async () => {
   const env = setupSmartWeaveEnv({});
   const caller = '<justin>';
-  assert.throws(
-    () =>
-      sell(env)(
-        {
-          ticker: 'FACTMKT',
-          title: '',
-          registry: '',
-          creator: '',
-          position: '',
-          creator_cut: 0,
-          balances: {},
-          oppose: {},
-          pair: '',
-          settings: [
-            ['communityLogo', 'dAsWVqq_lFqeVsc7Z7HvfZNh-kQBQAIcOpsDz6NBM80'],
-            ['isTradeable', true],
-          ],
-        },
-        { caller, input: {} }
-      ),
-    /position must be support or oppose./
-  );
+  try {
+    await sell(env)(
+      {
+        ticker: 'FACTMKT',
+        title: '',
+        registry: '',
+        creator: '',
+        position: '',
+        creator_cut: 0,
+        balances: {},
+        oppose: {},
+        pair: '',
+        settings: [
+          ['communityLogo', 'dAsWVqq_lFqeVsc7Z7HvfZNh-kQBQAIcOpsDz6NBM80'],
+          ['isTradeable', true],
+        ],
+      },
+      { caller, input: {} }
+    );
+    assert.unreachable();
+  } catch (error) {
+    assert.is(error.message, 'position must be support or oppose.');
+  }
 });
 
-test('should throw (position must be support or oppose.) if wrong', () => {
+test('should throw (position must be support or oppose.) if wrong', async () => {
   const env = setupSmartWeaveEnv({});
   const caller = '<justin>';
-  assert.throws(
-    () =>
-      sell(env)(
-        {
-          ticker: 'FACTMKT',
-          name: '',
-          balances: {},
-          settings: [
-            ['communityLogo', 'dAsWVqq_lFqeVsc7Z7HvfZNh-kQBQAIcOpsDz6NBM80'],
-            ['isTradeable', true],
-          ],
-        },
-        { caller, input: { position: 'suppor' } }
-      ),
-    /position must be support or oppose./
-  );
+  try {
+    await sell(env)(
+      {
+        ticker: 'FACTMKT',
+        title: '',
+        registry: '',
+        creator: '',
+        position: '',
+        creator_cut: 0,
+        balances: {},
+        oppose: {},
+        pair: '',
+        settings: [
+          ['communityLogo', 'dAsWVqq_lFqeVsc7Z7HvfZNh-kQBQAIcOpsDz6NBM80'],
+          ['isTradeable', true],
+        ],
+      },
+      { caller, input: { position: 'suppor' } }
+    );
+    assert.unreachable();
+  } catch (error) {
+    assert.is(error.message, 'position must be support or oppose.');
+  }
 });
 
-test('should throw (qty must be an integer greater than zero.) if undefined', () => {
+test('should throw (qty must be an integer greater than zero.) if undefined', async () => {
   const env = setupSmartWeaveEnv({});
   const caller = '<justin>';
-  assert.throws(
-    () =>
-      sell(env)(
-        {
-          ticker: 'FACTMKT',
-          title: '',
-          registry: '',
-          creator: '',
-          position: '',
-          creator_cut: 0,
-          balances: {},
-          oppose: {},
-          pair: '',
-          settings: [
-            ['communityLogo', 'dAsWVqq_lFqeVsc7Z7HvfZNh-kQBQAIcOpsDz6NBM80'],
-            ['isTradeable', true],
-          ],
-        },
-        { caller, input: { position: 'support' } }
-      ),
-    /qty must be an integer greater than zero./
-  );
+  try {
+    await sell(env)(
+      {
+        ticker: 'FACTMKT',
+        title: '',
+        registry: '',
+        creator: '',
+        position: '',
+        creator_cut: 0,
+        balances: {},
+        oppose: {},
+        pair: '',
+        settings: [
+          ['communityLogo', 'dAsWVqq_lFqeVsc7Z7HvfZNh-kQBQAIcOpsDz6NBM80'],
+          ['isTradeable', true],
+        ],
+      },
+      { caller, input: { position: 'support' } }
+    );
+    assert.unreachable();
+  } catch (error) {
+    assert.is(error.message, 'qty must be an integer greater than zero.');
+  }
 });
 
-test('should throw (qty must be an integer greater than zero.) if less than 0', () => {
+test('should throw (qty must be an integer greater than zero.) if less than 0', async () => {
   const env = setupSmartWeaveEnv({});
   const caller = '<justin>';
-  assert.throws(
-    () =>
-      sell(env)(
-        {
-          ticker: 'FACTMKT',
-          title: '',
-          registry: '',
-          creator: '',
-          position: '',
-          creator_cut: 0,
-          balances: {},
-          oppose: {},
-          pair: '',
-          settings: [
-            ['communityLogo', 'dAsWVqq_lFqeVsc7Z7HvfZNh-kQBQAIcOpsDz6NBM80'],
-            ['isTradeable', true],
-          ],
-        },
-        { caller, input: { position: 'support', qty: 0.1 } }
-      ),
-    /qty must be an integer greater than zero./
-  );
+  try {
+    await sell(env)(
+      {
+        ticker: 'FACTMKT',
+        title: '',
+        registry: '',
+        creator: '',
+        position: '',
+        creator_cut: 0,
+        balances: {},
+        oppose: {},
+        pair: '',
+        settings: [
+          ['communityLogo', 'dAsWVqq_lFqeVsc7Z7HvfZNh-kQBQAIcOpsDz6NBM80'],
+          ['isTradeable', true],
+        ],
+      },
+      { caller, input: { position: 'support', qty: 0.1 } }
+    );
+    assert.unreachable();
+  } catch (error) {
+    assert.is(error.message, 'qty must be an integer greater than zero.');
+  }
 });
 
-test('should throw (qty must be an integer greater than zero.) if string', () => {
+test('should throw (qty must be an integer greater than zero.) if string', async () => {
   const env = setupSmartWeaveEnv({});
   const caller = '<justin>';
-  assert.throws(
-    () =>
-      sell(env)(
-        {
-          ticker: 'FACTMKT',
-          title: '',
-          registry: '',
-          creator: '',
-          position: '',
-          creator_cut: 0,
-          balances: {},
-          oppose: {},
-          pair: '',
-          settings: [
-            ['communityLogo', 'dAsWVqq_lFqeVsc7Z7HvfZNh-kQBQAIcOpsDz6NBM80'],
-            ['isTradeable', true],
-          ],
-        },
-        { caller, input: { position: 'support', qty: '0.1' } }
-      ),
-    /qty must be an integer greater than zero./
-  );
+  try {
+    await sell(env)(
+      {
+        ticker: 'FACTMKT',
+        title: '',
+        registry: '',
+        creator: '',
+        position: '',
+        creator_cut: 0,
+        balances: {},
+        oppose: {},
+        pair: '',
+        settings: [
+          ['communityLogo', 'dAsWVqq_lFqeVsc7Z7HvfZNh-kQBQAIcOpsDz6NBM80'],
+          ['isTradeable', true],
+        ],
+      },
+      { caller, input: { position: 'support', qty: '0.1' } }
+    );
+    assert.unreachable();
+  } catch (error) {
+    assert.is(error.message, 'qty must be an integer greater than zero.');
+  }
 });
 
-test('should throw (Incorrect price.)', () => {
+test('should throw (Caller expected a different amount.)', async () => {
   const env = setupSmartWeaveEnv({});
   const caller = '<justin>';
-
-  assert.throws(
-    () =>
-      sell(env)(
-        {
-          ticker: 'FACTMKT',
-          title: '',
-          registry: '',
-          creator: '',
-          position: '',
-          creator_cut: 0,
-          balances: {
-            '<justin>': 1,
-            '<jshaw>': 99,
-          },
-          oppose: {},
-          pair: '',
-          settings: [
-            ['communityLogo', 'dAsWVqq_lFqeVsc7Z7HvfZNh-kQBQAIcOpsDz6NBM80'],
-            ['isTradeable', true],
-          ],
+  try {
+    await sell(env)(
+      {
+        ticker: 'FACTMKT',
+        title: '',
+        registry: '',
+        creator: '',
+        position: '',
+        creator_cut: 0,
+        balances: {
+          '<justin>': 1,
+          '<jshaw>': 99,
         },
-        {
-          caller,
-          input: {
-            position: 'support',
-            expected: 98,
-            qty: 1,
-          },
-        }
-      ),
-    /The price has changed./
-  );
-});
-
-test('should throw (Incorrect fee.)', () => {
-  const env = setupSmartWeaveEnv({});
-  const caller = '<justin>';
-  assert.throws(
-    () =>
-      sell(env)(
-        {
-          ticker: 'FACTMKT',
-          title: '',
-          registry: '',
-          creator: '',
-          position: '',
-          creator_cut: 0,
-          balances: {
-            '<jshaw>': 99,
-            '<justin>': 1,
-          },
-          oppose: {},
-          pair: '',
-          settings: [
-            ['communityLogo', 'dAsWVqq_lFqeVsc7Z7HvfZNh-kQBQAIcOpsDz6NBM80'],
-            ['isTradeable', true],
-          ],
-        },
-        {
-          caller,
-          input: {
-            position: 'support',
-            qty: 1,
-            price: 100,
-            fee: 4,
-            txId: '<txId>',
-          },
-        }
-      ),
-    /The price has changed./
-  );
-});
-
-test('should throw (Error: An error occurred while claiming the pair.) if claim doesnt work.', () => {
-  const env = setupSmartWeaveEnv({ write: false });
-  const caller = '<justin>';
-  const txId = '<txId>';
-  const pair = '<pair>';
-  assert.throws(
-    () =>
-      sell(env)(
-        {
-          ticker: 'FACTMKT',
-          title: '',
-          registry: '',
-          creator: '',
+        oppose: {},
+        pair: '',
+        settings: [
+          ['communityLogo', 'dAsWVqq_lFqeVsc7Z7HvfZNh-kQBQAIcOpsDz6NBM80'],
+          ['isTradeable', true],
+        ],
+      },
+      {
+        caller,
+        input: {
           position: 'support',
-          creator_cut: 0,
-          balances: {
-            '<jshaw>': 99,
-          },
-          oppose: {},
-          pair,
-          settings: [
-            ['communityLogo', 'dAsWVqq_lFqeVsc7Z7HvfZNh-kQBQAIcOpsDz6NBM80'],
-            ['isTradeable', true],
-          ],
+          expected: 98,
+          qty: 1,
         },
-        {
-          caller,
-          input: {
-            position: 'support',
-            qty: 1,
-            price: 100,
-            fee: 5,
-            txId,
-          },
-        }
-      ),
-    /Caller balance too low./
-  );
+      }
+    );
+    assert.unreachable();
+  } catch (error) {
+    assert.is(error.message, 'Caller expected a different amount.');
+  }
 });
 
-test('should sell 1 support token for 100 base units with a fee of 5 base units', () => {
-  const env = setupSmartWeaveEnv({ write: true });
+test('should throw (Caller balance too low.)', async () => {
+  const env = setupSmartWeaveEnv({});
   const caller = '<justin>';
-  const txId = '<txId>';
-  const pair = '<pair>';
-  const output = sell(env)(
-    {
-      ticker: 'FACTMKT',
-      title: '',
-      registry: '',
-      creator: '',
-      position: 'support',
-      creator_cut: 0,
-      balances: {
-        '<jshaw>': 99,
-        '<justin>': 1,
+  try {
+    await sell(env)(
+      {
+        ticker: 'FACTMKT',
+        title: '',
+        registry: '',
+        creator: '',
+        position: '',
+        creator_cut: 0,
+        balances: {
+          '<justin>': 0,
+          '<jshaw>': 99,
+        },
+        oppose: {},
+        pair: '',
+        settings: [
+          ['communityLogo', 'dAsWVqq_lFqeVsc7Z7HvfZNh-kQBQAIcOpsDz6NBM80'],
+          ['isTradeable', true],
+        ],
       },
-      oppose: {},
-      pair,
-      settings: [
-        ['communityLogo', 'dAsWVqq_lFqeVsc7Z7HvfZNh-kQBQAIcOpsDz6NBM80'],
-        ['isTradeable', true],
-      ],
-    },
-    {
-      caller,
-      input: {
-        position: 'support',
-        expected: 99,
-        qty: 1,
-      },
-    }
-  );
-  const { state } = output;
-
-  assert.is(state.creator_cut, 0);
-  assert.is(state.balances[caller], 0);
+      {
+        caller,
+        input: {
+          position: 'support',
+          qty: 1,
+        },
+      }
+    );
+  } catch (error) {
+    assert.is(error.message, 'Caller balance too low.');
+  }
 });
 
-test('should not distribute to creator', () => {
+test('should sell 1 support token for 100 base units', async () => {
   const env = setupSmartWeaveEnv({ write: true });
   const caller = '<justin>';
   const creator = 'test';
   const txId = '<txId>';
   const pair = '<pair>';
-  const output = sell(env)(
+  const output = await sell(env)(
     {
       ticker: 'FACTMKT',
       title: '',
@@ -320,16 +249,56 @@ test('should not distribute to creator', () => {
       caller,
       input: {
         position: 'support',
-        expected: 99,
+        expected: 100,
         qty: 1,
       },
     }
   );
+
   const { state } = output;
 
-  assert.is(state.creator_cut, 49000);
   assert.is(state.balances[caller], 0);
-  assert.is(env?.balances()['test'], undefined);
+});
+
+test('should sell 1 oppose token for 1 base units', async () => {
+  const env = setupSmartWeaveEnv({ write: true });
+  const caller = '<justin>';
+  const creator = 'test';
+  const txId = '<txId>';
+  const pair = '<pair>';
+  const output = await sell(env)(
+    {
+      ticker: 'FACTMKT',
+      title: '',
+      registry: '',
+      creator,
+      position: 'support',
+      creator_cut: 49000,
+      balances: {
+        '<jshaw>': 99,
+      },
+      oppose: {
+        '<justin>': 1,
+      },
+      pair,
+      settings: [
+        ['communityLogo', 'dAsWVqq_lFqeVsc7Z7HvfZNh-kQBQAIcOpsDz6NBM80'],
+        ['isTradeable', true],
+      ],
+    },
+    {
+      caller,
+      input: {
+        position: 'oppose',
+        expected: 1,
+        qty: 1,
+      },
+    }
+  );
+
+  const { state } = output;
+
+  assert.is(state.oppose[caller], 0);
 });
 
 test.run();
